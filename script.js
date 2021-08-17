8652acb7be64371f0bbdc27cd520ee93d28c9ef5
Bimalini <89073684+Bimalini@users.noreply.github.com>
2021-08-17 16:06:47 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,146 @@
+function check(){
+    var a,b,c,result;
+    b=Number(document.getElementById("selec").value);
+    c=Number(document.getElementById("selec2").value);
+    a=Number(document.getElementById("a").value);
+    if(b==c)
+    {
+      alert("You have entered Two Same country: Please try again!!!");
+    }
+    else
+    {
+    switch (b)
+    {
+    
+    <!-- *****************start case 1***************** -->
+     case 1:
+     {
+      if(b==1 && c==2)
+      {
+       result=83.70*a;
+      }
+      else if(b==1 && c==2)
+      {
+       result=64.94*a;
+      }
+      else if(b==1 && c==3)
+      {
+       result=112.11*a;
+      }
+      else if(b==1 && c==4)
+      {
+       result=0.85*a;
+      }
+      else if(b==1 && c==5)
+      {
+       result=0.85*a;
+      }
+      break;
+     }
+    <!-- *****************end case 1***************** -->
+    
+    
+    
+    <!-- *****************start case 2***************** -->
+     case 2:
+     {
+      if(b==2 && c==1)
+      {
+       result=0.01*a;
+      }
+      else if(b==2 && c==3)
+      {
+       result=0.78*a;
+      }
+      else if(b==2 && c==4)
+      {
+       result=1.34*a;
+      }
+      else if(b==2 && c==5)
+      {
+       result=0.01*a;
+      }
+      break;
+     }
+    <!-- *****************end case 2***************** -->
+    
+    
+    
+    <!-- *****************start case 3***************** -->
+     case 3:
+     {
+      if(b==3 && c==1)
+      {
+       result=0.02*a;
+      }
+      else if(b==3 && c==2)
+      {
+       result=1.29*a;
+      }
+      else if(b==3 && c==4)
+      {
+       result=1.73*a;
+      }
+      else if(b==3 && c==5)
+      {
+       result=0.01*a;
+      }
+      break;
+     }
+    <!-- *****************end case 3***************** -->
+    
+    
+    
+    <!-- *****************start case 4***************** -->
+     case 4:
+     {
+      if(b==4 && c==1)
+      {
+       result=0.01*a;
+      }
+      else if(b==4 && c==2)
+      {
+       result=0.75*a;
+      }
+      else if(b==4 && c==3)
+      {
+       result=0.58*a;
+      } 
+      else if(b==4 && c==5)
+      {
+       result=0.01*a;
+      }
+      break;
+     }
+    <!-- *****************end case 4***************** -->
+    
+    
+    
+    <!-- *****************start case 5***************** -->
+     case 5:
+     {
+      if(b==5 && c==1)
+      {
+       result=1.18*a;
+      }
+      else if(b==5 && c==2)
+      {
+       result=98.69*a;
+      }
+      else if(b==5 && c==3)
+      {
+       result=76.57*a;
+      }
+      else if(b==5 && c==4)
+      {
+       result=132.20*a;
+      }
+      break;
+     }
+    <!-- *****************end case 5***************** -->
+    
+    
+    } 
+    }
+    document.getElementById("answer").value=result;
+    }
